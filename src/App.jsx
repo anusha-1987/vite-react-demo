2518b3d300aeb0cdae14b7d777e91185001922f2
abondalapati <abondalapati@opentext.com>
2022-12-22 18:46:17 +0530

    add/substract button added

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,23 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
+  const [counter, SetCounter] = useState(0);
+useEffect(()=> {
+  //SetCounter(100); // The code inside of this useEffect() method only happens at the initial load of the component.
+  
+},[counter]); //simply providing the variable name here, whenever the variable value changed, we cna call code inside of useEffect() method
 
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
+      
       <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <a href='https://reactjs.org/docs/hooks-reference.html' name="React Ref">React Ref</a> <br></br>
+    Substract<button onClick={() => SetCounter((prevCount) => prevCount-1)}>-</button>
+    <h1>{counter}</h1>
+    Addition<button onClick={() => SetCounter((prevCount) => prevCount+1)}>+</button>
     </div>
   )
 }
